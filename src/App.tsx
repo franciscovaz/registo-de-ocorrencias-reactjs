3a35dfdd618cc7de8e09206b45d7cc905928399b
Francisco Vaz <ftomasvaz@gmail.com>
2020-05-27 23:05:38 +0100

    add global styles with styled components add routes, login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-const App: React.FC = () => {
-  return <h1>Registo de ocorrências</h1>;
-};
+import GlobalStyle from './styles/global';
+
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
