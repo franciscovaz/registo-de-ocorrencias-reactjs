680fd0cc38c4d4f38b20a6f03ade804facb3a7f8
Francisco Vaz <ftomasvaz@gmail.com>
2020-06-08 11:35:31 +0100

    adding unform to signup and input component

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
 
+import { Form } from '@unform/web';
 import imgLogo from '../../assets/logo.svg';
 
 import { Container, Content, Background } from './styles';
@@ -8,39 +9,45 @@ import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
 
-    <Content>
-      <img
-        src={imgLogo}
-        width="230"
-        height="134"
-        alt="Registo de Ocorrencias"
-      />
+  return (
+    <Container>
+      <Background />
 
-      <form>
-        <h1>Faça o seu registo</h1>
-
-        <Input name="name" icon={FiUser} type="name" placeholder="Name" />
-        <Input name="email" icon={FiMail} type="email" placeholder="Email" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Password"
+      <Content>
+        <img
+          src={imgLogo}
+          width="230"
+          height="134"
+          alt="Registo de Ocorrencias"
         />
 
-        <Button type="submit">Registar</Button>
-      </form>
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça o seu registo</h1>
 
-      <a href="/">
-        <FiArrowLeft />
-        Voltar ao Login
-      </a>
-    </Content>
-  </Container>
-);
+          <Input name="name" icon={FiUser} type="name" placeholder="Name" />
+          <Input name="email" icon={FiMail} type="email" placeholder="Email" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Password"
+          />
+
+          <Button type="submit">Registar</Button>
+        </Form>
+
+        <a href="/">
+          <FiArrowLeft />
+          Voltar ao Login
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
