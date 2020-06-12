70ca947cd6bd0e9f01fad893b7640e809f6fef5c
Francisco Vaz <ftomasvaz@gmail.com>
2020-06-12 10:00:01 +0100

    create user connected to API

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -11,25 +11,42 @@ import { Container, Content, AnimationContainer, Background } from './styles';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api';
+
+interface UserInfoToAdd {
+  nome_utilizador: string;
+  email_utilizador: string;
+  telemovel_utilizador?: string;
+  password_utilizador: string;
+  fk_tipo_utilizador: number; // utilizador comum - 4
+}
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const history = useHistory();
+
+  const handleSubmit = useCallback(async (data: UserInfoToAdd) => {
     formRef.current?.setErrors({});
     try {
       const schema = Yup.object().shape({
-        name: Yup.string().required('Nome obrigatório'),
-        email: Yup.string()
+        nome_utilizador: Yup.string().required('Nome obrigatório'),
+        email_utilizador: Yup.string()
           .required('Email obrigatório')
           .email('Digite um email válido'),
-        password: Yup.string().min(6, 'Mínimo de 6 caracteres'),
+        password_utilizador: Yup.string().min(6, 'Mínimo de 6 caracteres'),
       });
 
       await schema.validate(data, {
         abortEarly: false,
       });
+
+      data.fk_tipo_utilizador = 4;
+
+      await api.post('utilizador', data);
+
+      history.push('/');
     } catch (err) {
       formRef.current?.setErrors(getValidationErrors(err));
     }
@@ -51,15 +68,20 @@ const SignUp: React.FC = () => {
           <Form ref={formRef} onSubmit={handleSubmit}>
             <h1>Faça o seu registo</h1>
 
-            <Input name="name" icon={FiUser} type="name" placeholder="Name" />
             <Input
-              name="email"
+              name="nome_utilizador"
+              icon={FiUser}
+              type="name"
+              placeholder="Name"
+            />
+            <Input
+              name="email_utilizador"
               icon={FiMail}
               type="email"
               placeholder="Email"
             />
             <Input
-              name="password"
+              name="password_utilizador"
               icon={FiLock}
               type="password"
               placeholder="Password"
