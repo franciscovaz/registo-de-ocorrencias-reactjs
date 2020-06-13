0d34be8916a799b72fbca21288a39c9c89d39dd6
Francisco Vaz <ftomasvaz@gmail.com>
2020-06-13 11:33:54 +0100

    add listOccurrences component, create new route and change duration of toast to 5 sec

---
+++ src/pages/ListOccurrences/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const ListOccurrences: React.FC = () => {
+  return (
+    <Container>
+      <h1>Listagem</h1>
+    </Container>
+  );
+};
+
+export default ListOccurrences;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ import Route from './Route';
 import Login from '../pages/Login';
 import SignUp from '../pages/SignUp';
 import CreateOccurrence from '../pages/CreateOccurrence';
+import ListOccurrences from '../pages/ListOccurrences';
 
 const Routes: React.FC = () => (
   <>
@@ -14,6 +15,7 @@ const Routes: React.FC = () => (
       <Route path="/signup" component={SignUp} />
 
       <Route path="/create-occurrence" component={CreateOccurrence} isPrivate />
+      <Route path="/list-occurrences" component={ListOccurrences} isPrivate />
     </Switch>
   </>
 );
