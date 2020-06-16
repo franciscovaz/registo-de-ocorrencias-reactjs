b9f942698aed698a18ffaefaab4a1069ee28642a
Francisco Vaz <ftomasvaz@gmail.com>
2020-06-16 22:44:12 +0100

    create table

--- src/pages/ListOccurrences/index.tsx
+++ src/pages/ListOccurrences/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, TableContainer } from './styles';
 import Header from '../../components/Header';
 
 const ListOccurrences: React.FC = () => {
@@ -9,6 +9,54 @@ const ListOccurrences: React.FC = () => {
       <Header />
       <Container>
         <h1>Listagem</h1>
+
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Titulo</th>
+                <th>Utilizador</th>
+                <th>Comentário</th>
+                <th>Estado</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>Buraco na rua da mota</td>
+                <td>Tomas</td>
+                <td>Sem comentários</td>
+                <td>Em análise</td>
+                <td>29-03-2020</td>
+              </tr>
+
+              <tr>
+                <td>Passeio sem tampa de esgoto</td>
+                <td>Francisco</td>
+                <td>Colocada nova tampa</td>
+                <td>Terminado</td>
+                <td>20-04-2020</td>
+              </tr>
+
+              <tr>
+                <td>Contentor do lixo deslocado</td>
+                <td>Jose</td>
+                <td>Sem comentários</td>
+                <td>Por tratar</td>
+                <td>10-06-2020</td>
+              </tr>
+
+              <tr>
+                <td>Buraco na rua da sul</td>
+                <td>Vaz</td>
+                <td>Resolvido</td>
+                <td>Terminado</td>
+                <td>15-06-2020</td>
+              </tr>
+            </tbody>
+          </table>
+        </TableContainer>
       </Container>
     </>
   );
--- src/pages/ListOccurrences/styles.ts
+++ src/pages/ListOccurrences/styles.ts
@@ -1,3 +1,12 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+
+export const TableContainer = styled.section`
+  margin-top: 64px;
+
+  table {
+    width: 100%;
+    border-spacing: 0 8px;
+  }
+`;
